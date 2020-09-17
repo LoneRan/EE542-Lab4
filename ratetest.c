012854a8cb6fd17dc009395b03d05b95e0a16437
LoneRan <48748710+LoneRan@users.noreply.github.com>
2020-09-17 01:47:46 -0700

    Update ratetest.c

--- ratetest.c
+++ ratetest.c
@@ -73,11 +73,9 @@ int main(int argc, char **argv)
                 // printf("node %d received %d byte(s) from %d\n",
                 //     world_rank, transfer_num, partner_rank);
             }
-            if (transfer_count == transfer_LIMIT)
-            {
-                gettimeofday(&end_time, NULL);
-            }
+            
         }
+         gettimeofday(&end_time, NULL);
         elap = calTime(end_time, start_time);
         printf("It takes %ld ms to transfer %d bytes back and forth %d times\n", elap, transfer_num, transfer_LIMIT / 2);
         if (elap > 0)
