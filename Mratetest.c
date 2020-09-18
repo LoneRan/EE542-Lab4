5b861cf9d54f9d4692c2de1a2a505d53aa6948b0
LoneRan <48748710+LoneRan@users.noreply.github.com>
2020-09-17 22:05:35 -0700

    Update Mratetest.c

--- Mratetest.c
+++ Mratetest.c
@@ -80,7 +80,7 @@ int main(int argc, char **argv)
         elap = calTime(end_time, start_time);
 
         printf("It takes %ld ms to transfer %d bytes back and forth %d times\n", elap, transfer_num * power, transfer_LIMIT / 2);
-        long throughput = sizeof(MPI_INT) * (long)transfer_num * power * transfer_LIMIT * 1000 / elap;
+        long throughput = sizeof(MPI_INT) * (long)transfer_num * power * transfer_LIMIT * 500 / elap;
         printf("Throughput = %ld byte/s\n", throughput);
         power = power * 2;
     }
